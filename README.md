931140c750cea0cf3c0086377cd20985ad72c4a6
Giando <giando.olini@gogodigital.it>
2018-11-07 00:15:41 +0100

    Initial Release

--- README.md
+++ README.md
@@ -17,3 +17,64 @@ or add this line to the require section of your `composer.json` file.
 ```
 "cinghie/yii2-tinify": "^1.0.0"
 ```
+
+Usage
+-----------------
+
+Initialize component
+
+```
+$tinify = new Tinify(['apiKey' => 'YOUR API KEY']);
+```
+
+Compress image from Path
+
+```
+// create a new image
+$tinify->compress('path/to/file/to/compress','path/to/file/after/compression');
+// overwrite file
+$tinify->compress('path/to/file/to/compress');
+```
+
+Compress image from Buffer
+
+```
+$tinify->compressFromBuffer('path/to/file/to/compress');
+```
+
+Compress image from Url
+
+```
+$tinify->compressFromUrl('https://tinypng.com/images/panda-happy.png','path/to/file/after/compression');
+```
+
+Resize image
+
+```
+// create a new image
+$tinify->resize('path/to/file/to/compress','path/to/file/after/compression',['method' => 'fit', 'width' => 150, 'height' => 100]);
+// overwrite file
+$tinify->resize('path/to/file/to/compress',null,['method' => 'fit', 'width' => 150, 'height' => 100]);
+// Available methods
+scale,fit,cover,thumb
+```
+
+Store image to Amazon S3
+
+```
+$tinify = new Tinify([
+	'apiKey' => 'YOUR API KEY',
+	'aws_access_key_id' => 'AKIAIOSFODNN7EXAMPLE',
+    'aws_secret_access_key' => 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
+    'region' => 'us-west-1',
+    'headers' => array('Cache-Control' => 'max-age=31536000, public'),
+    'path' => 'example-bucket/my-images/optimized.jpg'
+]);
+$tinify->storeToAmazonS3('path/to/file/to/compress');
+```
+
+Compressions used this month
+
+```
+$count = $tinify->usage();
+```
---
+++ Tinify.php
@@ -0,0 +1,178 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Gogodigital Srls
+ * @company Gogodigital Srls - Wide ICT Solutions
+ * @website http://www.gogodigital.it
+ * @github https://github.com/cinghie/yii2-media
+ * @license BSD-3-Clause
+ * @package yii2-tinify
+ * @version 1.0.0
+ */
+
+namespace cinghie\tinify;
+
+use Tinify as BaseTinify;
+use yii\base\Component;
+use yii\base\InvalidConfigException;
+use yii\web\UnauthorizedHttpException;
+
+class Tinify extends Component
+{
+	/**
+	 * @var string $amazon_access_key_id
+	 */
+	public $amazon_access_key_id;
+
+	/**
+	 * @var string $amazon_secret_access_key
+	 */
+	public $amazon_secret_access_key;
+
+	/**
+	 * @var string $amazon_region
+	 */
+	public $amazon_region;
+
+	/**
+	 * @var string $amazon_headers
+	 */
+	public $amazon_headers;
+
+	/**
+	 * @var string $amazon_path
+	 */
+	public $amazon_path;
+
+	/**
+	 * @var string $apiKey
+	 */
+	public $apiKey;
+
+	/**
+	 * @var string $proxy
+	 */
+	public $proxy;
+
+	/**
+	 * Initialize Component
+	 *
+	 * @throws InvalidConfigException if $apiKey is not set
+	 * @throws UnauthorizedHttpException if $apiKey is not validated
+	 *
+	 * See [TinyPng docs](https://tinypng.com/developers/reference/php#authentication)
+	 */
+	public function init()
+	{
+		if (!$this->apiKey) {
+			throw new InvalidConfigException('The TinyPNG apiKey must be in set in ' . \get_class($this) . '.');
+		}
+
+		if($this->proxy) {
+			BaseTinify\setProxy($this->proxy);
+		}
+
+		try {
+			BaseTinify\setKey($this->apiKey);
+			BaseTinify\validate();
+		} catch(BaseTinify\Exception $e) {
+			throw new UnauthorizedHttpException('The TinyPNG apiKey '.$this->apiKey.' could not be validated.');
+		}
+	}
+
+	/**
+	 * Compress image from Path
+	 *
+	 * @param string $sourceImage
+	 * @param string $destinationImage
+	 *
+	 * See [TinyPng docs](https://tinypng.com/developers/reference/php#compressing-images)
+	 */
+	public function compress($sourceImage, $destinationImage = null)
+	{
+		$destinationImage = $destinationImage ?: $sourceImage;
+		$source = BaseTinify\fromFile($sourceImage);
+		$source->toFile($destinationImage);
+	}
+
+	/**
+	 * Compress image from Buffer
+	 *
+	 * @param string $bufferImage
+	 *
+	 * @return mixed
+	 *
+	 * See [TinyPng docs](https://tinypng.com/developers/reference/php#compressing-images)
+	 */
+	public function compressFromBuffer($bufferImage)
+	{
+		$sourceData = file_get_contents($bufferImage);
+
+		return BaseTinify\fromBuffer($sourceData)->toBuffer();
+	}
+
+	/**
+	 * Compress image from URL
+	 *
+	 * @param string $urlImage
+	 * @param string $destinationImage
+	 *
+	 * See [TinyPng docs](https://tinypng.com/developers/reference/php#compressing-images)
+	 */
+	public function compressFromURL($urlImage, $destinationImage)
+	{
+		$source = BaseTinify\fromUrl($urlImage);
+		$source->toFile($destinationImage);
+	}
+
+	/**
+	 * Resize image
+	 *
+	 * @param string $sourceImage
+	 * @param string $destinationImage
+	 * @param array $options
+	 *
+	 * - string method scale | fit | cover | thumb
+	 * - int width
+	 * - int height
+	 *
+	 * See [TinyPng docs](https://tinypng.com/developers/reference/php#resizing-images)
+	 */
+	public function resize($sourceImage, $destinationImage = null, $options = ['method' => 'fit', 'width' => 150, 'height' => 100])
+	{
+		$destinationImage = $destinationImage ?: $sourceImage;
+		$source  = BaseTinify\fromFile($sourceImage);
+		$resized = $source->resize($options);
+		$resized->toFile($destinationImage);
+	}
+
+	/**
+	 * Store image to Amazon S3
+	 *
+	 * @param $sourceImage
+	 *
+	 * See [TinyPng docs](https://tinypng.com/developers/reference/php#saving-to-amazon-s3)
+	 */
+	public function storeToAmazonS3($sourceImage)
+	{
+		$source = BaseTinify\fromFile($sourceImage);
+		$source->store(array(
+			'service' => 's3',
+			'aws_access_key_id' => $this->amazon_access_key_id,
+			'aws_secret_access_key' => $this->amazon_secret_access_key,
+			'region' => $this->amazon_region,
+			'headers' => $this->amazon_headers,
+			'path' => $this->amazon_path
+		));
+	}
+
+	/**
+	 * Gets compressions used this month
+	 *
+	 * See [TinyPng docs](https://tinypng.com/developers/reference/php#compression-count)
+	 */
+	public function compressCount()
+	{
+		return BaseTinify\compressionCount();
+	}
+}
